b5b4515909c9ab22c697a94b146b18ab292ab419
AzCraft <47869198+AstralShadow@users.noreply.github.com>
2025-08-01 04:53:23 +0300

    game: Render track

--- src/game/render.cpp
+++ src/game/render.cpp
@@ -1,4 +1,5 @@
 #include "game/game.hpp"
+#include "game/data.hpp"
 #include "core/core.hpp"
 #include <SDL2/SDL_render.h>
 
@@ -8,6 +9,10 @@ static auto& rnd = core::renderer;
 void game::render(scene_uid)
 {
     SDL_RenderClear(rnd);
+
+    auto& track = game::track(current_track);
+    SDL_RenderCopy(rnd, track.tex, nullptr, nullptr);
+
 
     SDL_RenderPresent(rnd);
 }
