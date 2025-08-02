4ce8de45b36d0627f1077978d9c325b431dcf8fb
AzCraft <47869198+AstralShadow@users.noreply.github.com>
2025-08-02 14:42:48 +0300

    game: Load resources in advance

--- src/game/init.cpp
+++ src/game/init.cpp
@@ -8,7 +8,12 @@ using std::endl;
 
 void game::init(int, char**, scene_uid)
 {
-    
+    // Preload static resources
+    for(size_t i = 0; i < CAR_LAST; i++)
+        car_type(static_cast<CarEnum>(i));
+
+    for(size_t i = 0; i < TRACK_LAST; i++)
+        track(static_cast<TrackEnum>(i));
 
 #ifndef __EMSCRIPTEN__
     tires = 20;
